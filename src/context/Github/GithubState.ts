ea1fd23a55af539bb4d159a15b3af6d47c0d3fd3
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 12:20:46 +0000

    finish github state

--- src/context/Github/GithubState.ts
+++ src/context/Github/GithubState.ts
@@ -30,4 +30,79 @@ const GithubState = (props) => {
         loading: false,
         initialUsers: [],
     }
-}
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+    // SEARCH USERS 
+    const searchUsers = async (text) => { 
+        setLoading();
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+
+    
+    dispatch({
+        type: SEARCH_USERS,
+        payload: res.data.items,
+    }); 
+    console.log(res.data.items)
+    };
+
+    // GET SINGLE USER 
+    const getSingleUser = async(username) => { 
+        setLoading();
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+        setLoading(false);
+        dispatch({
+            type: GET_USER,
+            payload: res.data
+        }); 
+        console.log(res.data)
+    }
+
+    // GET A USER'S GH REPOS
+    const getUserRepos = async (username) => {
+        setLoading();
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=creadted:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+        setLoading(false);
+        dispatch({
+            type: GET_USER,
+            payload: res.data
+        });
+        console.log(res.data)
+    }
+
+    // CLEAR USER'S FROM STATE 
+    const clearUsers = () => dispatch({ type: CLEAR_USERS })
+
+    // SET LOADING 
+    const setLoading = () => dispatch({ type: SET_LOADING })
+
+    // GET INIT USERS 
+    const getInitialUsers = async () => { 
+        setLoading();
+        const res = await axios.get(`https://api.github.com/search/users&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+
+    
+    dispatch({
+        type: GET_INIT_USERS,
+        payload: res.data.items,
+    }); 
+    console.log(res.data.items)
+    };
+    return (
+        <GithubContext.Provider 
+            value={{ 
+                users: state.users,
+                user: state.user,
+                repos: state.repos,
+                loading: state.loading,
+                searchUsers,
+                clearUsers,
+                getSingleUser,
+                getUserRepos,
+                getInitialUsers
+             }}
+             >
+             {props.children}
+             </GithubContext.Provider>
+    );
+};
+export default GithubState;
