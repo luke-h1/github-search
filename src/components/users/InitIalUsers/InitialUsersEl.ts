1b1b1a6693d9fd6ec8839c046cfa1e0ecb469a56
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 18:43:29 +0000

    fix styling

--- src/components/users/InitIalUsers/InitialUsersEl.ts
+++ src/components/users/InitIalUsers/InitialUsersEl.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     min-height: 50vh;
     max-width: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 15px;
 `;
