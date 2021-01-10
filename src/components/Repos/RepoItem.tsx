a4c2f58f44bb7c9d48cb20612f61f64bcc5d88b0
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 13:21:57 +0000

    updates

--- src/components/Repos/RepoItem.tsx
+++ src/components/Repos/RepoItem.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Card = styled.div``;
 
 const RepoItem = ({ repo }) => {
   return (
---
+++ src/context/Github/Search/Search.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from 'react';
+import GithubContext from '../githubContext';
+import AlertContext from '../../../context/Alert/alertContext';
+import {
+  SearchInput, Form, SearchInputBtn, Button, SearchWrap,
+} from './SearchEl';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+  const [text, setText] = useState('');
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      alertContext.setAlert('enter a valid string');
+    } else {
+      githubContext.searchUsers(text);
+      setText('');
+    }
+  };
+  return (
+    <SearchWrap>
+      <Form onSubmit={onSubmit}>
+        <SearchInput
+          type="text"
+          name="text"
+          placeholder="search github users"
+          value={text}
+          onChange={onChange}
+        />
+        <SearchInputBtn
+          type="submit"
+          value="Search"
+        />
+      </Form>
+      {githubContext.users.length > 0 && (
+      <Button onClick={githubContext.clearUsers}>
+        Clear
+      </Button>
+      )}
+    </SearchWrap>
+
+  );
+};
