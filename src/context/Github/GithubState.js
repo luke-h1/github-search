2be23ba51a1a4cdc22210e8e5c5fbdcf2b93c1cc
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 17:48:20 +0000

    clean githubState.js

--- src/context/Github/GithubState.js
+++ src/context/Github/GithubState.js
@@ -5,16 +5,6 @@ import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import { GET_INIT_USERS, SEARCH_USERS, CLEAR_USERS, SET_LOADING, GET_USER, GET_REPOS } from '../types';
 
-let githubClientId;
-let githubClientSecret;
-
-if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_CLIENT_ID;
-  githubClientSecret = process.env.REACT_APP_CLIENT_SECRET;
-} else {
-  console.log("we're in production 😎 😀 ");
-}
-
 const GithubState = props => {
   const initialState = {
     users: [],
