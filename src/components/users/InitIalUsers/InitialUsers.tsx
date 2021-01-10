54c32293cc3c2e4920ce1d3db4da9fbdba56aef7
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 12:55:18 +0000

    add loading comp

--- src/components/users/InitIalUsers/InitialUsers.tsx
+++ src/components/users/InitIalUsers/InitialUsers.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import axios from 'axios';
 import { Container } from './InitialUsersEl';
 import GithubContext from '../../../context/Github/githubContext';
-import Loading from '../layout/loading';
+import Loading from '../../layout/Loading';
 import UserItem from '../UserItem/UserItem';
 
 const InitialUsers = () => {
