2b4cf0bfb1785c7a065e55282d52967bec1bccc8
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 19:58:33 +0000

    refact nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
+import Header from './components/Header/Navbar';
 import { GlobalStyle } from './styles/GlobalStyles';
 import Alert from './components/layout/Alert';
 import About from './pages/About';
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,45 +1,31 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { ThemeProvider } from 'styled-components';
 import NavData from '../../data/nav.json';
-import {
-  Nav,
-  NavbarContainer,
-  NavLogo,
-  NavIcon,
-  MobileIcon,
-  NavMenu,
-  NavItem,
-  NavItemBtn,
-  NavLinks,
-  NavBtnLink,
-} from './NavbarElements';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavItemBtn, NavLinks, NavBtnLink } from './NavbarElements';
 
 interface NavInt {
-  title: string;
-  slug: Number;
+  title: any;
+  slug: any;
 }
 
-const Navbar: React.FC<{theme: String}> = (props) => {
-  const theme = props;
+const Navbar = props => {
   const [click, setClick] = useState(false);
   const handleClick: Object = () => setClick(!click);
   const closeMobileMenu: Object = () => setClick(false);
 
   return (
-    <ThemeProvider theme={theme}>
       <>
         <Nav>
           <NavbarContainer>
-            <NavLogo to="/" onClick={closeMobileMenu}>
+            <NavLogo to='/' onClick={closeMobileMenu}>
               <NavIcon />
               lhowsam
             </NavLogo>
-            <MobileIcon onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-            </MobileIcon>
+            <MobileIcon onClick={handleClick}>{click ? <FaTimes /> : <FaBars />}</MobileIcon>
             <NavMenu onClick={handleClick} click={click}>
-              {NavData.map((item: NavInt) => (
+              {NavData.map(item => (
                 <NavItem>
                   <NavLinks to={item.slug} onClick={closeMobileMenu}>
                     {item.title}
@@ -54,7 +40,6 @@ const Navbar: React.FC<{theme: String}> = (props) => {
           </NavbarContainer>
         </Nav>
       </>
-    </ThemeProvider>
   );
 };
 
