c118d87efd2f731e51491909d52fc67d8a57a84c
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 20:12:37 +0000

    fix about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react';
 import styled from 'styled-components';
+import { FiGithub } from 'react-icons/fi';
 
 const AboutWrap = styled.div`
   display: flex;
@@ -46,6 +47,20 @@ const SocialLinks = styled.div`
   text-align: center;
 `;
 
+const GithubLink = styled(FiGithub)`
+  font-size: 50px;
+  color: #000;
+  cursor: pointer;
+  &:hover { 
+    color: #0090C1;
+  }
+`;
+
+const StyledA = styled.a`
+  color: #000;
+  text-decoration: none;
+`;
+
 const About = () => {
   return (
     <AboutWrap>
@@ -62,7 +77,21 @@ const About = () => {
               Utilizes Node JS, Express, Babel & jest to run an express server in order to hide sensitive github API keys + secrets. This server makes calls to the github API and returns that to the React frontend to consume
             </Item>
           </List>
-          <SocialLinks />
+          <SocialLinks>
+            <Title>Source code / Github repos:</Title>
+          </SocialLinks>
+          <SocialLinks>
+            <Title>Frontend:</Title>
+            <StyledA href="https://github.com/luke-h1/github-search">
+              <GithubLink />
+            </StyledA>
+          </SocialLinks>
+          <SocialLinks>
+            <Title>Backend:</Title>
+            <StyledA href="https://github.com/luke-h1/github-api">
+              <GithubLink />
+            </StyledA>
+          </SocialLinks>
         </AboutContent>
       </AboutDesc>
     </AboutWrap>
