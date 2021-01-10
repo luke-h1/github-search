f006d6dc137b370c104effcbe8b62791752fc426
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 13:39:39 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,28 @@ import { GlobalStyle } from './styles/GlobalStyles';
 import Alert from './components/layout/Alert';
 import About from './pages/About';
 import UserItem from './components/users/UserItem/UserItem';
+import GithubState from './context/Github/GithubState';
+import AlertState from './context/Alert/AlertState';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <GlobalStyle />
-        <Header />
-        <Switch>
-          <Route />
-        </Switch>
-      </>
-    </Router>
-
+    <GithubState>
+      <AlertState>
+        <Router>
+          <GlobalStyle />
+          <Header />
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route path="/user/:login" component={UserItem} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 };
 
