6deaa6ddb2079fda461a8826007119901f167e30
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 19:02:33 +0000

    add deploy scripts

---
+++ deploy-to-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#!/bin/bash
+if ! npm run lint; then
+    echo ''
+    echo ''
+    echo "###########################################" 
+    echo "# ❌     ESLINT returned an error.     ❌  #" 
+    echo "# ❌ process exited with status code 1 ❌  #" 
+    echo "###########################################" 
+    exit 1
+else
+    echo 'eslint returned no errors. Deploying to production ✅'
+fi
+rm -rf build/ 
+npm run build 
+vc 
+vc --prod
---
+++ deploy-to-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if ! npm run lint; then
+    echo ''
+    echo ''
+    echo "###########################################" 
+    echo "# ❌     ESLINT returned an error.     ❌  #" 
+    echo "# ❌ process exited with status code 1 ❌  #" 
+    echo "###########################################" 
+    exit 1
+else
+    echo 'eslint returned no errors. Deploying to production ✅'
+fi
+rm -rf build/ 
+npm run build 
+vc 
