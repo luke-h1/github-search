5e55090818ecacd874c436338562254edbd9d295
luke-h1 <luke.howsam@yahoo.com>
2021-01-10 18:50:05 +0000

    lint + fix link

--- src/components/users/UserItem/UserItem.tsx
+++ src/components/users/UserItem/UserItem.tsx
@@ -3,31 +3,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  UserWrap,
-  UserLink,
-  Title,
-  BtnWrap,
-  UserLinkExternal,
-} from './UserItemEl';
+import { UserWrap, UserLink, Title, BtnWrap, UserLinkExternal } from './UserItemEl';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <>
       <UserWrap>
-        <img
-          src={avatar_url}
-          style={{ width: '280px', borderRadius: '10px' }}
-        />
+        <img src={avatar_url} style={{ width: '280px', borderRadius: '10px' }} />
         <Title />
         <BtnWrap>
-          <UserLink to={`/user/${login}`}
-        >More Details</UserLink>
-       <UserLinkExternal href={`${html_url}`}
-       target='_blank'
-            style={{ marginTop: '1rem' }}
-        >Github Profile</UserLinkExternal>
-
+          <UserLink to={`/user/${login}`}>More Details</UserLink>
+          <UserLinkExternal href={`${html_url}`} target='_blank' rel='noreferrer noopener' style={{ marginTop: '1rem' }}>
+            Github Profile
+          </UserLinkExternal>
         </BtnWrap>
       </UserWrap>
     </>
